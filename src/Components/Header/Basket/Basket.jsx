323e1cd64a722199cd74b8ce6f3dc106b8b45a65
May Hoang <mayhoang@MBP-de-May.cust.communityfibre.co.uk>
2023-12-08 16:45:01 +0000

    add basket icon

--- src/Components/Header/Basket/Basket.jsx
+++ src/Components/Header/Basket/Basket.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 import "./Basket.css";
+import basket from "../../../assets/basket.png"
 
 const Basket = () => {
   return (
     <Link className="basket-link" to={"/basket"}>
-      Basket
+      {/* Basket */}
+      <img src={basket} className="basket" alt="basket" />
     </Link>
   );
 };
